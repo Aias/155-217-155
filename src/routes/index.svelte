9be6d2ceeacc8be2f4ebc08f3fef8d9c9df831ab
Nick Trombley <trombley.nick@gmail.com>
2019-08-05 23:07:09 -0400

    landing page layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -20,8 +20,60 @@
 	<title>thirty(one) days of poetry</title>
 </svelte:head>
 
-{#each poems as {slug, meta}}
-<div>
-	<a href="{`/${slug}`}">{meta.title}</a>
-</div>
-{/each}
+<header>
+	<h2>
+		<span>one&nbsp;hundred&nbsp;fifty-five</span> /
+		<span>two&nbsp;hundred&nbsp;seventeen</span> /
+		<span>one&nbsp;hundred&nbsp;fifty-five</span>
+	</h2>
+	<h3>stupid poems for a stupid girl</h3>
+</header>
+
+<main>
+	{#each poems as {slug, meta, html}}
+	<article>
+		<h4 href="{`/${slug}`}">{meta.title}</h4>
+		<section>
+			{@html html}
+		</section>
+		<small>{31 - meta.order}</small>
+	</article>
+	{/each}
+</main>
+
+<style>
+	header {
+		text-align: center;
+		margin-bottom: 2rem;
+	}
+
+	h2 > span {
+		white-space: nowrap;
+	}
+
+	h3 {
+		font-weight: 200;
+	}
+
+	main {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+		grid-gap: 1rem;
+	}
+
+	article {
+		padding: 2rem 2rem;
+		border: 1px solid black;
+		position: relative;
+	}
+
+	small {
+		position: absolute;
+		top: 0.25rem;
+		right: 0.5rem;
+	}
+
+	section > :global(*:last-child) {
+		margin-bottom: 0;
+	}
+</style>
