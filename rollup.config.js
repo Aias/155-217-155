b353a2771fd737a98469a337ac5ca2b6865f2d74
Nick Trombley <trombley.nick@gmail.com>
2021-05-10 20:03:10 -0400

    dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,10 @@
-import resolve from 'rollup-plugin-node-resolve';
-import replace from 'rollup-plugin-replace';
-import commonjs from 'rollup-plugin-commonjs';
+import path from 'path';
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import commonjs from '@rollup/plugin-commonjs';
+import url from '@rollup/plugin-url';
 import svelte from 'rollup-plugin-svelte';
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import config from 'sapper/config/rollup.js';
 import pkg from './package.json';
@@ -12,11 +14,9 @@ const dev = mode === 'development';
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
 const onwarn = (warning, onwarn) =>
-	(warning.code === 'CIRCULAR_DEPENDENCY' &&
-		/[/\\]@sapper[/\\]/.test(warning.message)) ||
+	(warning.code === 'MISSING_EXPORT' && /'preload'/.test(warning.message)) ||
+	(warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) ||
 	onwarn(warning);
-const dedupe = importee =>
-	importee === 'svelte' || importee.startsWith('svelte/');
 
 export default {
 	client: {
@@ -24,24 +24,32 @@ export default {
 		output: config.client.output(),
 		plugins: [
 			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
+				preventAssignment: true,
+				values: {
+					'process.browser': true,
+					'process.env.NODE_ENV': JSON.stringify(mode)
+				}
 			}),
 			svelte({
-				dev,
-				hydratable: true,
-				emitCss: true
+				compilerOptions: {
+					dev,
+					hydratable: true
+				}
+			}),
+			url({
+				sourceDir: path.resolve(__dirname, 'src/node_modules/images'),
+				publicPath: '/client/'
 			}),
 			resolve({
 				browser: true,
-				dedupe
+				dedupe: ['svelte']
 			}),
 			commonjs(),
 
 			legacy &&
 				babel({
 					extensions: ['.js', '.mjs', '.html', '.svelte'],
-					runtimeHelpers: true,
+					babelHelpers: 'runtime',
 					exclude: ['node_modules/@babel/**'],
 					presets: [
 						[
@@ -68,6 +76,7 @@ export default {
 				})
 		],
 
+		preserveEntrySignatures: false,
 		onwarn
 	},
 
@@ -76,23 +85,32 @@ export default {
 		output: config.server.output(),
 		plugins: [
 			replace({
-				'process.browser': false,
-				'process.env.NODE_ENV': JSON.stringify(mode)
+				preventAssignment: true,
+				values: {
+					'process.browser': false,
+					'process.env.NODE_ENV': JSON.stringify(mode)
+				}
 			}),
 			svelte({
-				generate: 'ssr',
-				dev
+				compilerOptions: {
+					dev,
+					generate: 'ssr',
+					hydratable: true
+				},
+				emitCss: false
+			}),
+			url({
+				sourceDir: path.resolve(__dirname, 'src/node_modules/images'),
+				publicPath: '/client/',
+				emitFiles: false // already emitted by client build
 			}),
 			resolve({
-				dedupe
+				dedupe: ['svelte']
 			}),
 			commonjs()
 		],
-		external: Object.keys(pkg.dependencies).concat(
-			require('module').builtinModules ||
-				Object.keys(process.binding('natives'))
-		),
-
+		external: Object.keys(pkg.dependencies).concat(require('module').builtinModules),
+		preserveEntrySignatures: 'strict',
 		onwarn
 	}
 };
